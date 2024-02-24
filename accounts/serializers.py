1dd3dcb859475aac503dee245697af87c4efeade
Davi Castro <davialmeida47jd@gmail.com>
2024-02-24 13:48:08 -0300

    feat(accounts): created initial serializers, views to accounts app

---
+++ accounts/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class RegisterSerializer(serializers.Serializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+
+        if User.objects.filter(username=data['username']).exists():
+            raise serializers.ValidationError('This username already exists')
+
+        return data
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        user.set_password(validated_data['password'])
+
+        return validated_data
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
+from rest_framework import status
 
-# Create your views here.
+
+def RegisterView(APIView):
+
+    def post(self, request):
+        try:
+            data = request.data
+
+            serializer = RegisterSerializer(data=data)
+
+            if not serializer.is_valid():
+                return Response(
+                    {
+                        'data': serializer.errors,
+                        'message': 'Some error occurred',
+                    },
+                    status=status.HTTP_400_BAD_REQUEST)
+
+            serializer.save()
+
+            return Response(
+                {
+                    'data': serializer.data,
+                    'message': 'User registered successfully',
+                },
+                status=status.HTTP_201_CREATED
+            )
+
+        except Exception:
+            return Response({
+                'data': None,
+                'message': 'Some error occurred',
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
